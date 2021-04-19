b064e380d4d48e200e9b252cd74cc07b78ba055d
Liz Thompson <thompsonel2011@gmail.com>
2021-04-19 17:27:39 -0400

    draft of script.js

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+// ADD QUERY SELECTORS FOR THE FOLLOWING: 
+var timer = '';
+var startButton = '';
+var question = '';
+var answerChoice0 = '';
+var answerChoice1 = '';
+var answerChoice2 = '';
+var answerChoice3 = '';
+
+var quiz = [{
+    question: 'What is The Dudes favorite drink?',
+    answerChoice0: 'Manhatten'
+    answerChoice1: 'Old Fashioned'
+    answerChoice2: 'White Russian'
+    answerChoice3: 'Arnold Palmer'
+    correctAnswer: 'White Russian'
+}
+{
+    question: 'What "really tied the room together"?'
+    answerChoice0: 'rug'
+    answerChoice1: 'painting'
+    answerChoice2: 'lamp'
+    answerChoice3:  'ropes'
+    correctAnswer: 'rug'
+}
+{
+    question: 'Fill in the blank: "This is not _____, this is bowling. There are rules.'
+    answerChoice0: 'prison'
+    answerChoice1: 'Nam'
+    answerChoice2: 'the circus'
+    answerChoice3: 'a playground'
+    correctAnswer: 'Nam'
+}
+{
+    question: 'Which of the following is *not* one of the alternative names The Dude suggests to The Big Lebowski?'
+    answerChoice0: 'His Dudeness'
+    answerChoice1: 'El Duderino'
+    answerChoice2: 'Duder'
+    answerChoice3: 'Mounsieur Dude'
+    correctAnswer: 'Mounsieur Dude'
+}
+{
+    question: 'Who kidnapped Bunny?'
+    answerChoice0: 'Bunny'
+    answerChoice1: 'The Big Lebowski'
+    answerChoice2: 'The Nihilists'
+    answerChoice3: 'Jackie Treehorn'
+}
+]
