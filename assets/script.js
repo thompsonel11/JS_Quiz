27bafd3ea26c5e57b8834f0fa390e2438ab6333f
Liz Thompson <thompsonel2011@gmail.com>
2021-04-23 15:58:25 -0400

    after working with Tom

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var timerEl = document.querySelector("#timer");
 var timer;
-var timeRemaining = 10;
+var timeRemaining = 50;
 var startScreen = document.querySelector("#startQuizButton");
 var startButtonEl = document.querySelector("#startButton");
 var questionEl = document.querySelector("#question");
@@ -11,63 +11,66 @@ var AC1El = document.querySelector("#answerChoice1");
 var AC2El = document.querySelector("#answerChoice2");
 var AC3El = document.querySelector("#answerChoice3");
 var currentQuestionIndex = 0;
-
+var allDoneEl = document.querySelector("#allDone");
 var totalScore = document.querySelector("#finalScore");
 var scoreCounter = 50;
-// var correctAnswer = 
-// var chosen = 
-
+var amountCorrect = 0;
+var titleEl = document.querySelector("#CodeQuizChallenge");
+var secondsLeftEl = document.querySelector("#secondsLeft");
+var submitScoreEl = document.querySelector("#submitScore")
+var submitButton = document.querySelector("#submit")
 
 // Define all questions, answer choices and identify correct answer
-var quiz = [{
-    question: 'What is The Dudes favorite drink?',
-    AC0: 'Manhatten',
-    AC1: 'Old Fashioned',
-    AC2: 'White Russian',
-    AC3: 'Arnold Palmer',
-    correctAnswer: 'two'
-},
-{
-    question: 'What "really tied the room together"?',
-    AC0: 'rug',
-    AC1: 'painting',
-    AC2: 'lamp',
-    AC3:  'ropes',
-    correctAnswer: 'zero'
-},
-{
-    question: 'Fill in the blank: "This is not _____, this is bowling. There are rules.',
-    AC0: 'prison',
-    AC1: 'Nam',
-    AC2: 'the circus',
-    AC3: 'a playground',
-    correctAnswer: 'one'
-},
-{
-    question: 'Which of the following is *not* one of the alternative names The Dude suggests to The Big Lebowski?',
-    AC0: 'His Dudeness',
-    AC1: 'El Duderino',
-    AC2: 'Duder',
-    AC3: 'Mounsieur Dude',
-    correctAnswer: 'three'
-},
-{
-    question: 'Who kidnapped Bunny?',
-    AC0: 'Bunny',
-    AC1: 'The Big Lebowski',
-    AC2: 'The Nihilists',
-    AC3: 'Jackie Treehorn',
-    correctAnswer: 'zero'
-}
+var quiz = [
+     {
+        question: 'What is The Dudes favorite drink?',
+        AC0: 'Manhatten',
+        AC1: 'Old Fashioned',
+        AC2: 'White Russian',
+        AC3: 'Arnold Palmer',
+        correctAnswer: 'White Russian'
+    },
+    {
+        question: 'What "really tied the room together"?',
+        AC0: 'rug',
+        AC1: 'painting',
+        AC2: 'lamp',
+        AC3:  'ropes',
+        correctAnswer: 'rug'
+    },
+    {
+        question: 'Fill in the blank: "This is not _____, this is bowling. There are rules.',
+        AC0: 'prison',
+        AC1: 'Nam',
+        AC2: 'the circus',
+        AC3: 'a playground',
+        correctAnswer: 'Nam'
+    },
+    {
+        question: 'Which of the following is *not* one of the alternative names The Dude suggests to The Big Lebowski?',
+        AC0: 'His Dudeness',
+        AC1: 'El Duderino',
+        AC2: 'Duder',
+        AC3: 'Mounsieur Dude',
+        correctAnswer: 'Mounsieur Dude'
+    },
+    {
+        question: 'Who kidnapped Bunny?',
+        AC0: 'Bunny',
+        AC1: 'The Big Lebowski',
+        AC2: 'The Nihilists',
+        AC3: 'Jackie Treehorn',
+        correctAnswer: 'Bunny'
+    }
 ];
 
-// Event Listener to start the game + trigger timer
+// DONE Event Listener to start the game + trigger timer
 startButton.addEventListener("click", function() {
     startTimer();
     showCurrentQuestion();
 })
 
-// Function that creates timer
+// DONE Function that creates timer
 function startTimer () {
     timer = setInterval(function (){
         timeRemaining--;
@@ -79,12 +82,13 @@ function startTimer () {
     }, 1000);  
 }
 
-// Function to display question + answer choices
+// DONE Function to display question + answer choices
 function showCurrentQuestion () {
-    if (currentQuestionIndex >= quiz.length - 1) {
+    if (currentQuestionIndex > quiz.length - 1) {
         clearInterval(timer);
+        console.log(currentQuestionIndex)
         displayQuizResults();
-        currentQuestionIndex = quiz.length - 1;
+        // currentQuestionIndex = quiz.length - 1;
     }
 
     if(quiz[currentQuestionIndex] !== undefined) {
@@ -99,81 +103,37 @@ function showCurrentQuestion () {
     }
 }
 
-// IN PROGRESS Function to render questions + assess answer APPROACH 1 (YOUTUBE VIDEO - utilizes buttons in HTML)
-function renderQuestions() {
-   if(currentQuestionIndex > ) {
-
-   } else {
-    
-    let x = quiz[currentQuestionIndex];
-    question.innerHTML = "<p>" + x.question + "</p>";
-    AC0.innerHTML = x.AC0;
-    AC1.innerHTML = x.AC1;
-    AC2.innerHTML = x.AC2;
-    AC3.innerHTML = x.AC3;}
-}
-currentQuestionIndex++
-renderQuestions()
-
-
-
-// IN PROGRESS Function to render questions + assess answer APPROACH 2
-function answerIsCorrect(questionMetadata, answerKey) {
-    const correctAnswer = questionMetadata.correctAnswer;
-    return questionMetadata[answerKey] === correctAnswer;
-}
-currentQuestionIndex++
-renderQuestions()
-
-
-// NEED TO CREATE: Function to display the quiz results 
-function displayQuizResults() {
-    //TODO: put something here
-}
-
-
-
-
-// NEED TO CREATE: Functions to change score for correct/incorrect and display message to user 'Correct! or Incorrect!' 
-for (var currentQuestionIndex = 0; currentQuestionIndex < quiz.length; currentQuestionIndex++) {
-    choiceButton[questionIndex].addEventListener("click", function(event){
-
+// DONEish (unless I want to add on stuff for scoring here) Function to compare user selection to correct answer  
+function checkAnswer(event){
+    if(event.target.textContent === quiz[currentQuestionIndex].correctAnswer) {
+        console.log('Correct!')
+    } else {
+        console.log('Incorrect!')
+        timeRemaining = timeRemaining - 10
     }
-    )
-
+        currentQuestionIndex++
+        showCurrentQuestion()
 }
 
-function correctAnswer() {
-    NOTSUREWHATGOESHERE.textContent = "Correct!";
-}
-function incorrectAnswer () {
-    ORHERE.textContent = "Incorrect!";
-    scoreCounter--;
-}
+// Display Quiz Results
+function displayQuizResults () {
+    console.log('results')
+    questionEl.hidden = true;
+    answerChoiceContainer.hidden = true;
+    totalScore.hidden = false;
+    allDoneEl.hidden = false;
+    titleEl.hidden = true;
 
+    if (timeRemaining < 0){
+        timeRemaining = 0
+    } 
 
-// NEED TO CREATE: function to calculate final score and open form for user to input initials. This will need to access local storage. 
-
-function tallyResult(questionMetadata, selectedAnswer) {
-    if(answerIsCorrect(questionMetadata, selectedAnswer)) {
-        scoreCounter++;
-    }
-
-    questionIndex++;
-    showCurrentQuestion();
+    timerEl.textContent = timeRemaining + ' seconds';
+    secondsLeftEl.textContent = timeRemaining;
+    submitScoreEl.hidden = false;
 }
 
-
-
-choiceButton.addEventListener("click", function(event){
+submitButton.addEventListener("click", function(event){
     event.preventDefault();
-    console.log(event)
-    console.log('button clicked')
-    // var allChoices = document.getElementsByClassName('choice')
-
-    // if (e.target.matches(allChoices[1].getAttribute('id'))) {
-    //     tallyResult (quiz[currentQuestionIndex], "AC0")
-    // }
-
-    // console.log(scoreCounter);
+    window.location.replace("./highScores.html")
 })
